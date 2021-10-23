f25a3407fcdc31551da21bc0d2fd9e8d756146de
shiva chittiprolu <shivachittiprolu@gmail.com>
2021-10-23 18:25:45 +0530

    index.js for components export

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as NavBar } from './NavBar';
+export { default as Footer } from './Footer';
+export { default as HomePage } from './HomePage';
+export { default as Exchanges } from './Exchanges';
+export { default as CryptoCurrencies } from './CryptoCurrencies';
+export { default as CryptoDetails} from './CryptoDetails';
+export { default as News } from './News';
